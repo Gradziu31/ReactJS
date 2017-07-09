0d8612fe5fdd1b45007b649e942c28a04b48f7fc
Krystian G <bandzior_1994@tlen.pl>
2017-07-10 01:43:13 +0200

    dodanie dynamicznej tabeli informacyjnej, podłaczenie osobno styli

--- app.jsx
+++ app.jsx
@@ -2,13 +2,15 @@ var data = {
     title: 'Temat Kursu',
     description: 'Opis Kursu...',
     image: 'http://lorempixel.com/200/200/',
+    author: 'Testowy Autor',
+    deration: '6 godz',
     is_new: true,
     is_promo: false
 }
 
 var course = (
     <div className="media border">
-        {/* Zdjęcie */}
+        {/* Zdjęcie */} 
         <div className="media-left">
             <img src={data.image} alt="cover" />
         </div>
@@ -19,6 +21,22 @@ var course = (
             {/* Promocja */}
             <p>{data.description}</p> 
             {data.is_promo? <b>Kurs jest w Pomocji</b> : null}
+        </div>
+        
+        {/* Tabela informacyjna */}
+        <div className="media-right">
+        <table className="table course_details">
+            <tbody>
+            <tr>
+                <th>Autor</th>
+                <td>{data.author}</td>
+            </tr>
+            <tr>
+                <th>Czas Trwania</th>
+                <td>{data.deration}</td>
+            </tr>
+            </tbody>
+        </table>
         </div>
     </div>
             )
